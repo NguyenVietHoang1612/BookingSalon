d03bdd24e201441d95ee305466d5abb233837b9f
Hoang <153740062+NguyenVietHoang1612@users.noreply.github.com>
2025-12-25 16:43:46 +0700

    Fix name column Ui FixTimeSlot

--- obj/Debug/net9.0/BookingSalon.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookingSalon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingSalon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a28ce07c9358ca7a5aa2d1adab682cf747b6e26e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d25e4b4ffd8c5ccaf3d2e7ac52f247253ea80bfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingSalon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingSalon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
