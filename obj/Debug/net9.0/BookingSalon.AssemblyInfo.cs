3247d08f531d4a14663174c3ae8842b4e150819b
Hoang <153740062+NguyenVietHoang1612@users.noreply.github.com>
2026-01-08 17:33:21 +0700

    fix minor error

--- obj/Debug/net9.0/BookingSalon.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookingSalon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingSalon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1497e3ce05fd53cc7636da5b00435aa0986c4508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+965cbb377f4f72d4fbd4800b86a4cfe095146b64")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingSalon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingSalon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
